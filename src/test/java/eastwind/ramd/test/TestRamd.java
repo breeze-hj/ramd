a203ded14faf9de990a22a55dd8873a5d5a6a390
breeze-hj <huangjian_hdu@163.com>
2019-01-15 22:45:27 +0800

    test

--- src/test/java/eastwind/ramd/test/TestRamd.java
+++ src/test/java/eastwind/ramd/test/TestRamd.java
@@ -12,23 +12,27 @@ public class TestRamd {
 		String[] addresses = { ":18727", ":18728", ":18729" };
 		String allAddressesStr = String.join(",", addresses);
 
+		// start 3 applications at ports 18727,18728,18729
 		RamdApplication[] applications = new RamdApplication[addresses.length];
 		for (int i = 0; i < addresses.length; i++) {
 			applications[i] = RamdApplication.create(addresses[i], allAddressesStr);
 		}
 
-		String name = "test1";
-		putIfAbsent(applications[0], name);
-		putIfAbsent(applications[1], name);
+		String name = "test1"; // namespace
+		String key = "key1";
+		// only one application can put value of test1:key1
+		putIfAbsent(applications[0], name, key);
+		putIfAbsent(applications[1], name, key);
 
-		TimeUnit.MILLISECONDS.sleep(20);
+		TimeUnit.MILLISECONDS.sleep(1000); // after some time
+		// any other application can get the target value
 		RamdClient ramdClient = applications[2].newRamdClient(name);
-		ramdClient.get("key1").thenAccept(v -> System.out.println(v));
+		ramdClient.get(key).thenAccept(v -> System.out.println(v));
 	}
 
-	private static void putIfAbsent(RamdApplication application, String name) {
+	private static void putIfAbsent(RamdApplication application, String name, String key) {
 		RamdClient ramdClient = application.newRamdClient(name);
-		CompletableFuture<Boolean> cf = ramdClient.putIfAbsent("key1", k -> "value of " + k);
+		CompletableFuture<Boolean> cf = ramdClient.putIfAbsent(key, k -> "value of " + k);
 		cf.thenAccept(t -> {
 			String message = String.format("application of %s put %s", application.getAddressStr(), t);
 			System.out.println(message);
